f7e847bca08161252972b731a43df96b9ef37c46
shpat konjusha <shpat@Ardits-MacBook-Pro.local>
2022-07-19 00:49:12 +0200

    added experience and about us sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Nav from './components/nav/Nav'
 import Header from './components/header/Header.jsx'
 import Footer from './components/footer/Footer'
+import Experience from './components/experience/Experience'
+import About from './components/about/About.jsx'
 
 
 
@@ -8,7 +10,9 @@ const App = () => {
   return (
     <>
     <Header />
-    <Nav />
+    <Nav/>
+    <About/>
+    <Experience/>
     <Footer/>
 
     </>
